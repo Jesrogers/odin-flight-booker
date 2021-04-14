5d325b3fe317a76235b77c23d2564a603f894707
Jesse Rogers <jesserileyrogers@gmail.com>
2021-04-13 23:17:54 -0400

    search form

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
     belongs_to :from_airport, class_name: "Airport", foreign_key: "from_airport_id"
     belongs_to :to_airport, class_name: "Airport", foreign_key: "to_airport_id"
+
+    def format_date
+        depature_date.strftime('%m/%d/%Y')
+    end
 end
