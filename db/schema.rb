97ef3ea359c2f3dc75b03c9cc5d1afc7773d229b
Jesse Rogers <jesserileyrogers@gmail.com>
2021-04-17 11:16:49 -0400

    migrations and BookingsController

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_143323) do
+ActiveRecord::Schema.define(version: 2021_04_17_151316) do
 
   create_table "airports", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_04_17_143323) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "flight_id"
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -36,6 +38,10 @@ ActiveRecord::Schema.define(version: 2021_04_17_143323) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "booking_id"
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
+  add_foreign_key "bookings", "flights"
+  add_foreign_key "passengers", "bookings"
 end
