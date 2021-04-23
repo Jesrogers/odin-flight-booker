3409b83a3b290cbe3c78a1f0671a5874157c3715
Jesse Rogers <jesserileyrogers@gmail.com>
2021-04-23 16:16:35 -0400

    ability to create booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+    include BookingsHelper
+
+    def new
+        @booking = Booking.new
+        @flight = Flight.find(params[:flight])
+        passenger_count = params[:passenger_count].to_i
+        passenger_count.times { @booking.passengers.build }
+    end
+
+    def create
+        @booking = Booking.new(booking_params)
+
+        if @booking.save
+            flash[:notice] = 'Booking created!'
+            redirect_to booking_path(@booking)
+        else
+            flash[:alert] = 'Error creating booking'
+            render :new
+        end
+    end
+
+    def show
+        @booking = Booking.find(params[:id])
+    end
 end
